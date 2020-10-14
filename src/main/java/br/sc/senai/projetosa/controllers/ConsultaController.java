fdbd9525f4578eb8310ae660e2923b398ef6c85a
Carlosouzapsi <carlosouzapsi@gmail.com>
2020-10-13 21:32:04 -0300

    adicionando listas no controller consultas

--- src/main/java/br/sc/senai/projetosa/controllers/ConsultaController.java
+++ src/main/java/br/sc/senai/projetosa/controllers/ConsultaController.java
@@ -27,6 +27,30 @@ public class ConsultaController {
 		return "cadastros/cadastroConsulta";
 		
 	}
+	/*
+	@GetMapping("/listarPacientes")
+	public String listarPacientes(Model model) {
+		try {
+			model.addAttribute("paciente", pacienteService.listarPacientes());
+			
+		}
+		catch(Exception e){
+			System.out.println("Erro: " + e.getMessage());
+		}
+		return "listas/listaDePacientes";
+		
+	}
+	*/
+	@GetMapping("/listarConsultas")
+	public String listarConsultas(Model model) {
+		try {
+			model.addAttribute("consulta", consultaService.listarConsultas());
+		}
+		catch(Exception e) {
+			System.out.println("Erro: " + e.getMessage());
+		}
+		return "listas/listaDeConsultas";
+	}
 	
 	@PostMapping("/salvar")
 	public String salvar(Consulta consulta) {
