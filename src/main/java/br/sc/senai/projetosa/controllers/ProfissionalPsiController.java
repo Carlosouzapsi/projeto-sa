73ba064371210f962fdf2b24befc3a57174d5293
Carlosouzapsi <carlosouzapsi@gmail.com>
2020-11-20 15:12:39 -0300

    trabalhando no modal de exclusão de lista

--- src/main/java/br/sc/senai/projetosa/controllers/ProfissionalPsiController.java
+++ src/main/java/br/sc/senai/projetosa/controllers/ProfissionalPsiController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -80,7 +81,7 @@ public class ProfissionalPsiController {
 	}
 	
 	@GetMapping("/excluir/{idPro}")
-	public String excluirProfissionalPsi(ProfissionalPsi profissionalPsi) {
+	public String excluirProfissionalPsi(@PathVariable("id") Long id, ProfissionalPsi profissionalPsi, ModelMap model) {
 		try {
 			profissionalPsiService.excluir(profissionalPsi);
 		}
